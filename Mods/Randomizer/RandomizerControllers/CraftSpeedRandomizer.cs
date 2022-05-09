3de2bbb41cc62887de372592d98d77dfba9889fb
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2022-05-09 17:26:25 +0200

    Rework craft speed randomizer patch

--- Mods/Randomizer/RandomizerControllers/CraftSpeedRandomizer.cs
+++ Mods/Randomizer/RandomizerControllers/CraftSpeedRandomizer.cs
@@ -1,7 +1,9 @@
 ﻿using GRandomizer.Util;
 using HarmonyLib;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace GRandomizer.RandomizerControllers
@@ -15,7 +17,7 @@ namespace GRandomizer.RandomizerControllers
 
         static readonly InitializeOnAccessDictionary<TechType, float> _craftTimes = new InitializeOnAccessDictionary<TechType, float>(key =>
         {
-            return (float)Math.Round(Mathf.Pow(UnityEngine.Random.value, 6f) * 60f, 1);
+            return (float)Math.Round((Mathf.Pow(UnityEngine.Random.value, 6f) + (1f / 60f)) * 60f, 1);
         });
 
         [HarmonyPatch]
@@ -23,15 +25,37 @@ namespace GRandomizer.RandomizerControllers
         {
             static MethodBase TargetMethod()
             {
-                float _float;
-                return SymbolExtensions.GetMethodInfo(() => CraftData.GetCraftTime(default, out _float));
+                return SymbolExtensions.GetMethodInfo(() => CraftData.GetCraftTime(default, out Discard<float>.Value));
             }
 
-            static void Postfix(bool __result, TechType techType, ref float result)
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase method)
             {
-                if (__result && IsEnabled())
+                int techTypeArgIndex = method.FindArgumentIndex(typeof(TechType));
+                int resultArgIndex = method.FindArgumentIndex(typeof(float).MakeByRefType());
+
+                foreach (CodeInstruction instruction in instructions)
                 {
-                    result = _craftTimes[techType];
+                    if (instruction.opcode == OpCodes.Ret)
+                    {
+                        yield return new CodeInstruction(OpCodes.Dup); // Dup return value
+                        yield return new CodeInstruction(OpCodes.Ldarg, techTypeArgIndex);
+                        yield return new CodeInstruction(OpCodes.Ldarg, resultArgIndex);
+                        yield return new CodeInstruction(OpCodes.Call, Hooks.Postfix_MI);
+                    }
+
+                    yield return instruction;
+                }
+            }
+
+            static class Hooks
+            {
+                public static readonly MethodInfo Postfix_MI = SymbolExtensions.GetMethodInfo(() => Postfix(default, default, ref Discard<float>.Value));
+                static void Postfix(bool __result, TechType techType, ref float result)
+                {
+                    if (__result && IsEnabled())
+                    {
+                        result = _craftTimes[techType];
+                    }
                 }
             }
         }
