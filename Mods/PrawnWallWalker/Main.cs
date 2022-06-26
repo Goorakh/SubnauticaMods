9162027c8e566faf3a588786b158ba5e31b4d2e5
Petter Öhlén <petter.ohlen@gmail.com>
2022-06-26 18:02:39 +0200

    Start adding "PrawnWallWalkerMod"

---
+++ Mods/PrawnWallWalker/Main.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrawnWallWalker
+{
+    [QModCore]
+    public class Main
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            string modName = ($"gorakh_{assembly.GetName().Name}");
+            Utils.LogInfo($"Patching {modName}");
+            Harmony harmony = new Harmony(modName);
+            harmony.PatchAll(assembly);
+            Utils.LogInfo($"{harmony.GetPatchedMethods().Count()} methods patched successfully!");
+        }
+    }
+}
---
+++ Mods/PrawnWallWalker/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace PrawnWallWalker
+{
+    public static class Utils
+    {
+        public static void LogError(string log, bool showOnScreen = false, int stackOffset = 0)
+        {
+            logLevel(QModManager.Utility.Logger.Level.Error, log, showOnScreen, stackOffset);
+        }
+
+        public static void LogInfo(string log, bool showOnScreen = false, int stackOffset = 0)
+        {
+            logLevel(QModManager.Utility.Logger.Level.Info, log, showOnScreen, stackOffset);
+        }
+
+        public static void LogWarning(string log, bool showOnScreen = false, int stackOffset = 0)
+        {
+            logLevel(QModManager.Utility.Logger.Level.Warn, log, showOnScreen, stackOffset);
+        }
+
+#if VERBOSE
+        public static void DebugLog(string log, bool showOnScreen = false, int stackOffset = 0)
+        {
+            logLevel(QModManager.Utility.Logger.Level.Debug, log, showOnScreen, stackOffset);
+        }
+#endif
+
+        static void logLevel(QModManager.Utility.Logger.Level level, string log, bool showOnScreen, int stackOffset)
+        {
+            MethodBase callerMethod = new StackTrace().GetFrame(2 + stackOffset)?.GetMethod();
+            string methodTag = callerMethod != null ? $"{callerMethod.DeclaringType.Name}.{callerMethod.Name}" : "null";
+
+            QModManager.Utility.Logger.Log(level, $"[{methodTag}]: {log}", null, showOnScreen);
+        }
+    }
+}
